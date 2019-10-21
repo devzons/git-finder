839496fb40490f975353d0b390a9689cce7d1e96
Don Yu <devzons@gmail.com>
2019-10-21 04:16:38 -0700

    finalize github finder app

--- src/App.js
+++ src/App.js
@@ -1,57 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
+import User from './components/users/User'
 import Alert from './components/layout/Alert'
-import axios from 'axios'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
+
+import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+
 import './App.css'
 
-class App extends Component {
-	state = {
-		users: [],
-		loading: false,
-		alert: null
-	}
-
-	// Search Github users
-	searchUsers = async text => {
-		this.setState({ loading: true })
-
-		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		)
-
-		this.setState({ users: res.data.items, loading: false })
-	}
-
-	// clear users from state
-	clearUsers = () => this.setState({ users: [], loading: false })
-
-	setAlert = (msg, type) => {
-		this.setState({ alert: { msg, type } })
-
-		setTimeout(() => this.setState({ alert: null }), 3000)
-	}
-
-	render() {
-		const { users, loading } = this.state
-
-		return (
-			<div className='App'>
-				<Navbar />
-				<div className='container'>
-					<Alert alert={this.state.alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={users.length > 0 ? true : false}
-						setAlert={this.setAlert}
-					/>
-					<Users loading={loading} users={users} />
-				</div>
-			</div>
-		)
-	}
+const App = () => {
+	return (
+		<GithubState>
+			<AlertState>
+				<Router>
+					<div className='App'>
+						<Navbar />
+						<div className='container'>
+							<Alert />
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route exact path='/about' component={About} />
+								<Route exact path='/user/:login' component={User} />
+								<Route component={NotFound} />
+							</Switch>
+						</div>
+					</div>
+				</Router>
+			</AlertState>
+		</GithubState>
+	)
 }
 
 export default App
